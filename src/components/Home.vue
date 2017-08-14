7f822b7c1c72ea30265e9f1e51f23bf3489735dd
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-08-14 10:43:43 +0200

    Add psd logo, add click table cell

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,17 +2,17 @@
   .home
     h1 {{ msg }}
 
-    el-table(:data='tableData', border='', style='width: 100%')
+    el-table(:data='tableData', border='', style='width: 100%', @cell-click="celda")
       el-table-column(label='Name', prop="name")
       el-table-column(label='Puntos')
         template(scope="scope")
           .tags
-            el-tag {{ scope.row.likes }}
+            el-tag.mr-5 {{ scope.row.likes }}
             el-tag(type="danger") {{ scope.row.dislikes }}
       el-table-column(label='Operations')
         template(scope='scope')
-          el-button(size='small', @click='handleEdit(scope.$index, scope.row)') Edit
-          el-button(size='small', type='danger', @click='handleDelete(scope.$index, scope.row)') Delete
+          el-button(size='small', icon="arrow-up", @click='handleEdit(scope.$index, scope.row)')
+          el-button(size='small', icon="arrow-down" type='danger', @click='handleDelete(scope.$index, scope.row)')
 
 
 </template>
@@ -24,14 +24,13 @@
     name: 'Home',
     mounted() {
       const usersRef = db.ref( 'users' );
-      console.debug( usersRef );
       usersRef.on( 'value', ( snapshot ) => {
-        console.debug( snapshot.val() );
         // Add Id
         this.tableData = this.$_( snapshot.val() ) // wrap object so that you can chain lodash methods
           .mapValues( ( value, id ) => this.$_.merge( {}, value, { id } ) ) // attach id to object
           .values() // get the values of the result
           .value(); // unwrap array of objects
+        console.debug( this.tableData );
       } );
     },
     data() {
@@ -40,12 +39,23 @@
         tableData: null,
       };
     },
+    methods: {
+      celda( row, column, cell, event ) {
+        if ( column.label === 'Name' ) {
+          console.debug( row, column, cell, event );
+          this.$router.push( { name: 'User', params: { id: row.id } } );
+        }
+      },
+    },
   };
 </script>
 
 <style lang="stylus" scoped>
   h1, h2
     font-weight normal
+
+  .mr-5
+    margin-right 5px
 
   a
     color #42b983
